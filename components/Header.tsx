8cbb2ed62a5bb62ec1b9303a6f56c9328ac63970
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2022-06-06 14:08:36 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Image from "next/image";
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+      <div className="flex items-center mx-7 xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="flex-1 ml-2 hidden lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      <form className="flex flex-1 item-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          className="flex-1 bg-transparent outline-none"
+          placeholder="Search reddit"
+        />
+        <button hidden className="" type="submit" />
+      </form>
+      <div className="flex text-gray-500 space-x-2 items-center mx-5 hidden lg:inline-flex">
+        <GlobeIcon className="icon" />
+        <SparklesIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="w-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+
+      <div className="ml-5 flex items-center lg:hidden">
+        <MenuIcon className="icon" />
+      </div>
+
+      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer ">
+        <div className="relative h-5 w-5 flex-shrink-0 ">
+          <Image
+            src="https://links.papareact.com/23l"
+            alt=""
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <p className="text-gray-400">Sign In</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
