1e3b96937d63ca4587abc019d3bc6d91c45168e6
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2022-06-10 15:21:57 +0200

    database implementation

--- components/Postbox.tsx
+++ components/Postbox.tsx
@@ -1,25 +1,63 @@
 import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "./Avatar";
 import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/client";
+import { ADD_POST, ADD_SUBREDDIT } from "../graphql/mutations";
+import client from "../apollo-client";
+import { GET_SUBREDDIT_BY_TOPIC } from "../graphql/queries";
 
 type FormData = {
   postTitle: string;
   postBody: string;
   postImage: string;
-  subrredit: string;
+  subreddit: string;
 };
 function Postbox() {
   const { data: session } = useSession();
+  const [addPosts] = useMutation(ADD_POST);
+  const [addSubreddit] = useMutation(ADD_SUBREDDIT);
+
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(async (formData) => {
+    console.log(formData);
+
+    try {
+      const {
+        data: { getSubredditListByTopic },
+      } = await client.query({
+        query: GET_SUBREDDIT_BY_TOPIC,
+        variables: {
+          topic: formData.subreddit,
+        },
+      });
+
+      const subredditExists = getSubredditListByTopic.length > 0;
+      if (!subredditExists) {
+        //
+        await addSubreddit({
+          variables: {
+            topic: formData.subreddit,
+          },
+        });
+      } else {
+        //
+      }
+    } catch (error) {}
+  });
   return (
-    <form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2 ">
+    <form
+      onSubmit={onSubmit}
+      className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2 "
+    >
       <div className="flex items-center space-between-x-3">
         <Avatar />
 
@@ -34,9 +72,67 @@ function Postbox() {
               : "You need to sign in to post"
           }
         />
-        <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 text-gray-300 cursor-pointer ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
         <LinkIcon className="h-6 text-gray-300" />
       </div>
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">SubReddit</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...(register("subreddit"), { required: true })}
+              type="text"
+              placeholder="I.e Next.JS"
+            />
+          </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="optional"
+              />
+            </div>
+          )}
+
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === "required" && (
+                <p>- A Post Title is required.</p>
+              )}
+              {errors.subreddit?.type === "required" && (
+                <p>- A Subreddit is required.</p>
+              )}
+            </div>
+          )}
+          {watch("postTitle") && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create Post
+            </button>
+          )}
+        </div>
+      )}
     </form>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,13 @@ import Postbox from "../components/Postbox";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit Clone by Shahin F</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Postbox />
-      <div>feed</div>
+      <div className="flex">feed</div>
     </div>
   );
 };
